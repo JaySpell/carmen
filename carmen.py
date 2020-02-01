bdba01bf2c1e9f5c4dc593d8fea79285dbe3d395
Jeremy Spell <jspell@gmail.com>
2020-02-01 13:28:07 -0600

    Added render_template to imports

--- carmen.py
+++ carmen.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import requests
 
 app = Flask(__name__)
@@ -13,7 +13,7 @@ def where_in_the_world():
     city_info = ""
     r = requests.get('https://ipinfo.io/json')
     city_info = r.json()['city']
-    return city_info
+    return render_template("index.html", city=city_info)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
